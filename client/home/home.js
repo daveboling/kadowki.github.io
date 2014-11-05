4cf1ac77e78a4ed20efd0bb73189b666ba02e933
Dave Boling <kadowki@gmail.com>
2014-11-04 23:28:08 -0600

    Slider with portfolio is now functional

--- client/home/home.js
+++ client/home/home.js
@@ -2,6 +2,20 @@
   'use strict';
   var home = angular.module('dave');
 
-  home.controller('HomeCtrl', ['$scope', '$location', '$anchorScroll', function($scope, $location, $anchorScroll){
+  home.controller('HomeCtrl', ['$scope', function($scope){
+
+    $scope.slider = function(){
+      $('#slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });
+    };
+
+    //run initial slider
+    $scope.slider();
+
   }]);
 })();
