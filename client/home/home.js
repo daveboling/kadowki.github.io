da1cd1ce5622e8bddc41720c43f1e981c391382d
Dave Boling <kadowki@gmail.com>
2014-12-02 21:34:35 -0600

    Added new portfolio looks for projects and featured projects

--- client/home/home.js
+++ client/home/home.js
@@ -6,36 +6,6 @@
   home.controller('HomeCtrl', ['$scope', '$http', function($scope, $http){
     $scope.email = {name:'', email:'', body:'', phone:''};
 
-    var slider = $('#slider');
-    $scope.slider = function(){
-      slider.slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: false,
-        autoplay: true,
-        autoplaySpeed: 6000
-      });
-    };
-
-
-
-    $scope.sliderNext = function(){
-      slider.slickNext();
-    };
-
-    $scope.sliderPrev = function(){
-      slider.slickPrev();
-    };
-
-    //run initial slider
-    var waiter = setTimeout(function(){
-     $scope.slider();
-     clearTimeout(waiter);
-    }, 500);
-
-
     $scope.sendMail = function(){
       return $http.post('http://api.daviddboling.com/sendMail', $scope.email).then(function(res){
         $scope.email = {name:'', email:'', body:'', phone:''};
@@ -44,7 +14,6 @@
         alertify.error('Sorry, something went wrong! Please try again.');
       });
     };
-
 
   }]);
 })();
